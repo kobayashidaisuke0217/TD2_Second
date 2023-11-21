95fabce0aa19373c1a0a5f19a32db569f5c8c95d
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-11-21 14:57:31 +0900

    ビームの判定を修正

--- game/Enemy/BeamEnemy.cpp
+++ game/Enemy/BeamEnemy.cpp
@@ -31,7 +31,7 @@ void BeamEnemy::Initialize(const Transform& transform, const Vector3& velocity, 
 void BeamEnemy::Update()
 {
 	Matrix4x4 rotateMatrix = MakeRotateMatrix(Vector3{ 0.0f,0.0f,0.0f });
-	obb_.size = { worldTransform_.scale_.x / 2.0f ,worldTransform_.scale_.y / 2.0f ,worldTransform_.scale_.z / 2.0f };
+	obb_.size = { worldTransform_.scale_.x / 2.0f ,worldTransform_.scale_.y  ,worldTransform_.scale_.z / 2.0f };
 	obb_.center = worldTransform_.translation_;
 	GetOrientations(rotateMatrix, obb_.orientation);
 	if (!ishit_) {
