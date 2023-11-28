84e8c66c9acdba9df0daa297c4855511716b90fc
shimazaki-ryuta <k022g0024@g.neec.ac.jp>
2023-11-28 15:37:10 +0900

    画像のシャギー修正

--- Resource/hlsl/Object3d.PS.hlsl
+++ Resource/hlsl/Object3d.PS.hlsl
@@ -10,7 +10,9 @@ PixelShaderOutput main(VertexShaderOutput input) {
 	float4 transformedUV = mul(float32_t4(input.texcoord, 0.0f, 1.0f),gMaterial.uvTransform);
 	float32_t4 textureColor = gTexture.Sample(gSampler, transformedUV.xy);
 
-
+	if (textureColor.a <= 0.1) {
+		discard;
+	}
 	/*output.color = gMaterial.color*  textureColor;*/
 	if (gMaterial.enableLighting != 0) {//lightingする場合
 		float Ndotl = dot(normalize(input.normal), -gDirectionalLight.direction);
