c930e15a1f3a6966b4310f185798bea22dd43d24
k022g0019@g.neec.ac.jp <k022g0019@g.neec.ac.jp>
2023-11-30 02:55:01 +0900

    狙い弾修正

--- game/Enemy/AimBulletEnemy.cpp
+++ game/Enemy/AimBulletEnemy.cpp
@@ -36,6 +36,10 @@ void AimBulletEnemy::Update()
 {
   currentCollTime_++;
   worldTransform_.rotation_.y += currentAtackCount_ / 15.0f;
+  Matrix4x4 rotateMatrix = MakeRotateMatrix(Vector3{ 0.0f,0.0f,0.0f });
+  obb_.size = { worldTransform_.scale_.x / 2.0f ,worldTransform_.scale_.y / 2.0f ,worldTransform_.scale_.z / 2.0f };
+  obb_.center = worldTransform_.translation_;
+  GetOrientations(rotateMatrix, obb_.orientation);
   if (player_) {
 		worldTransform_.translation_.x = player_->GetWorldTransform().translation_.x;
 	}
--- game/Enemy/ReflectEnemy.cpp
+++ game/Enemy/ReflectEnemy.cpp
@@ -34,10 +34,10 @@ void ReflectEnemy::Initialize(const Transform& transform, const Vector3& velocit
 	type_ = kReflect;
 	model_=model;
 	model_->setIsLighting(false);
-	//velocity_ = Subtract(player_->GetWorldTransform().GetWorldPos(), worldTransform_.translation_);
+	velocity_ = Subtract(player_->GetWorldTransform().GetWorldPos(), worldTransform_.translation_);
 	velocity_ = Normalise(velocity_);
 	velocity_ = Multiply(MoveSpeed_, velocity_);
-	worldTransform_.UpdateMatrix();
+	//worldTransform_.UpdateMatrix();
 	currentTime_=0;
 }
 
@@ -45,14 +45,14 @@ void ReflectEnemy::Update()
 {
 	currentTime_++;
 	if (currentTime_ >= startCount_) {
-		prePos_ = worldTransform_.translation_;
+		//prePos_ = worldTransform_.translation_;
 		Matrix4x4 rotateMatrix = MakeRotateMatrix(Vector3{ 0.0f,0.0f,0.0f });
 		obb_.size = { worldTransform_.scale_.x / 2.0f ,worldTransform_.scale_.y / 2.0f ,worldTransform_.scale_.z / 2.0f };
 		obb_.center = worldTransform_.translation_;
 		GetOrientations(rotateMatrix, obb_.orientation);
 		worldTransform_.translation_ = Add(worldTransform_.translation_, velocity_);
 
-		worldTransform_.UpdateMatrix();
+		
 		if (ishit_ == true) {
 			cooltime_++;
 		}
@@ -72,14 +72,14 @@ void ReflectEnemy::Update()
 				ishit_ = true;
 				reflectCount_++;
 				velocity_.y *= -1.0f;
-				worldTransform_.translation_ = prePos_;
+			//	worldTransform_.translation_ = prePos_;
 			}
 
 		}
 
 	}
 		
-		
+	worldTransform_.UpdateMatrix();
 }
 
 void ReflectEnemy::Draw(const ViewProjection& viewProjection)
@@ -97,7 +97,7 @@ void ReflectEnemy::isCollision(OBB pertner)
 				if (collisionpartner_ == kflore) {
 					if (std::abs(obb_.center.y - pertner.center.y) <= worldTransform_.scale_.y * 2.0f) {
 						velocity_.y *= -1.0f;
-						worldTransform_.translation_ = prePos_;
+						//worldTransform_.translation_ = prePos_;
 						if (obb_.center.y >= pertner.center.y) {
 							worldTransform_.translation_.y = pertner.center.y + pertner.size.y + worldTransform_.scale_.y;
 
